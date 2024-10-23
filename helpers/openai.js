1819f0abfe0e9124547cb29e54f606043d05be0d
Giolvani <inavloig@gmail.com>
2024-10-23 14:25:57 -0300

    get user to logging

--- helpers/openai.js
+++ helpers/openai.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 import logger from "./logger.js";
 import dotenv from "dotenv";
-import { UserDatabase } from './userDatabase.js'; // Importando a função
+import { UserDatabase } from './userDatabase.js';
 
 dotenv.config();
 
@@ -20,8 +20,9 @@ export async function createThread(userId) {
 
     if (!user || !user.thread_id) {
       const newThread = await client.beta.threads.create({
-        assistant_id: process.env.ASSISTANT_ID,
-        user_id: userId,
+        metadata: {
+          user_id: userId,
+        }
       });
 
       logger.info(`New thread created for user ${userId}: ${newThread.id}`);
@@ -44,21 +45,28 @@ export async function createThread(userId) {
 
 export async function addMessageToThread(threadId, userMessage) {
   try {
+    const userDatabase = new UserDatabase();
+    const user = userDatabase.getUserByThreadId(threadId);
+
     const message = await client.beta.threads.messages.create(threadId, {
       role: "user",
       content: userMessage,
+      metadata: {
+        user_id: user.id,
+      }
     });
 
-    logger.info(`Message sent to thread ${threadId}: ${message.id}`);
+    logger.info(`Message sent to user ${user.id} in thread ${threadId}: ${message.id}`);
     return message;
   } catch (error) {
-    logger.error("Error sending message to thread: " + (error.response ? error.response.data : error.message));
+    logger.error(`Error sending message to user ${user.id} in thread ${threadId}: ${error.response ? error.response.data : error.message}`);
     throw error;
   }
 }
 
 export async function getThreadMessage(threadId, messageId) {
   try {
+    logger.info(`Retrieving message ${messageId} from thread ${threadId}`);
     return await client.beta.threads.messages.retrieve(threadId, messageId);
   } catch (error) {
     logger.error("Error getting thread message: " + (error.response ? error.response.data : error.message));
@@ -68,6 +76,7 @@ export async function getThreadMessage(threadId, messageId) {
 
 export async function getThreadMessages(threadId, limit = 20, order = "desc") {
   try {
+    logger.info(`Retrieving messages from thread ${threadId} with limit ${limit} and order ${order}`);
     return await client.beta.threads.messages.list(threadId, { query: { limit, order } });
   } catch (error) {
     logger.error("Error getting thread messages: " + (error.response ? error.response.data : error.message));
@@ -77,6 +86,7 @@ export async function getThreadMessages(threadId, limit = 20, order = "desc") {
 
 export async function deleteThreadMessage(threadId, messageId) {
   try {
+    logger.info(`Deleting message ${messageId} from thread ${threadId}`);
     return await client.beta.threads.messages.del(threadId, messageId);
   } catch (error) {
     logger.error("Error deleting thread message: " + (error.response ? error.response.data : error.message));
@@ -86,10 +96,18 @@ export async function deleteThreadMessage(threadId, messageId) {
 
 export async function runThread(threadId) {
   try {
+    const userDatabase = new UserDatabase();
+    const user = userDatabase.getUserByThreadId(threadId);
+
+    logger.info(`Running thread ${threadId}`);
     const run = await client.beta.threads.runs.create(threadId, {
       assistant_id: process.env.ASSISTANT_ID,
+      metadata: {
+        user_id: user.id,
+      }
     });
 
+    logger.info(`Retrieving run ${run.id} from thread ${threadId}`);
     let retrieve = await client.beta.threads.runs.retrieve(threadId, run.id);
 
     while (['queued', 'in_progress', 'cancelling'].includes(retrieve.status)) {
@@ -109,4 +127,3 @@ export async function runThread(threadId) {
     throw error;
   }
 }
-
