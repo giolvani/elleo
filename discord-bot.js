7fdf83163f958f5547199a2d1574a90fbd0bc3db
Giolvani <inavloig@gmail.com>
2025-01-23 01:28:05 -0300

    update discord js lib version

--- discord-bot.js
+++ discord-bot.js
@@ -1,10 +1,11 @@
 import {
   Client,
   SlashCommandBuilder,
-  Events,
   GatewayIntentBits,
   Partials,
-  ChannelType
+  ChannelType,
+  REST,
+  Routes
 } from 'discord.js';
 import {
   createThread,
@@ -28,45 +29,66 @@ const client = new Client({
 
 const userDatabase = new UserDatabase();
 
-client.once(Events.ClientReady, (c) => {
-  console.log(`Bot is ready as ${c.user.id} :: ${c.user.username} [${c.user.tag}]`);
+const commands = [
+  new SlashCommandBuilder().setName('elleo-ping').setDescription('Replies with Pong!'),
+  new SlashCommandBuilder().setName('elleo-emoji').setDescription('Replies with an emoji!'),
+  new SlashCommandBuilder()
+    .setName('elleo-clear-history')
+    .setDescription('Clear the chat history!'),
+  new SlashCommandBuilder()
+    .setName('elleo-timezone')
+    .setDescription('Configure your timezone.')
+    .addStringOption((option) =>
+      option
+        .setName('timezone')
+        .setDescription(
+          'Your current timezone: "America/New_York". See more: https://timezonedb.com/time-zones'
+        )
+        .setRequired(true)
+    )
+].map((command) => command.toJSON());
 
-  const ping = new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!');
+client.once('ready', async () => {
+  console.log(`Bot is ready as ${client.user.tag}`);
 
-  const emoji = new SlashCommandBuilder().setName('emoji').setDescription('Replies with an emoji!');
+  // Registrar comandos aqui, pois client.application.id estará disponível
+  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-  const clearUserHistory = new SlashCommandBuilder()
-    .setName('clear-history')
-    .setDescription('Clear the chat history!');
-
-  client.application.commands.create(ping);
-  client.application.commands.create(emoji);
-  client.application.commands.create(clearUserHistory);
+  try {
+    console.log('Refreshing application commands...');
+    await rest.put(
+      Routes.applicationCommands(client.application.id), // Para comandos globais
+      // Routes.applicationGuildCommands(client.application.id, 'GUILD_ID'), // Para uma guilda específica
+      { body: commands }
+    );
+    console.log('Successfully reloaded application commands.');
+  } catch (error) {
+    console.error('Error while reloading application commands:', error);
+  }
 });
 
-client.on(Events.InteractionCreate, async (interaction) => {
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   const { commandName } = interaction;
 
-  if (commandName === 'ping') {
+  if (commandName === 'elleo-ping') {
     await interaction.reply('Pong!');
-  }
-
-  if (commandName === 'emoji') {
+  } else if (commandName === 'elleo-emoji') {
     await interaction.reply('<:elleobot:1295449209197166613>');
-  }
-
-  if (commandName === 'clear-history') {
-    await userDatabase.backupUserId(interaction.user.id);
-    await interaction.reply('Histórico de conversa limpo!');
+  } else if (commandName === 'elleo-clear-history') {
+    // userDatabase.clearUserHistory(interaction.user.id);
+    await interaction.reply('Chat history cleared!');
+  } else if (commandName === 'elleo-timezone') {
+    const timezone = interaction.options.getString('timezone');
+    //userDatabase.updateUser(interaction.user.id, { timezone });
+    await interaction.reply(`Your timezone has been set to: ${timezone}`);
   }
 });
 
-client.on(Events.MessageCreate, async (message) => {
+client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
 
-  // accept only DMs (for now)
   if (message.channel.type !== ChannelType.DM) return;
 
   await message.channel.sendTyping();
